cc6c64567b0ff3f036684c5b5059b73e5a7c93f9
Groza Marius <mariusgroza520@gmail.com>
2023-04-03 15:29:44 +0300

    Person

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!!!!");
+        Person marius = new Person("Marius", 27, true);
+        Person raluca = new Person("Raluca", 25, true);
+        Person sami = new Person("Sami", 18, false);
+        printPerson(raluca);
+        printPerson(marius);
+        printPerson(sami);
     }
+
+    public static void printPerson(Person X) {
+        String name = X.getName();
+        int age = X.getAge();
+        boolean married = X.isMarried();
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(married);
+    }
+
+
+
+
 }
