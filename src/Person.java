fb77d9cac5785d80297a782097167ccc6013a637
Groza Marius <mariusgroza520@gmail.com>
2023-04-03 15:22:33 +0300

    curs6

---
+++ src/Person.java
@@ -0,0 +1,37 @@
+/*1) create an object that represents a Person
+
+state:
+
+- name
+
+- age
+
+- married
+
+behavior: getName, getAge, isMarried*/
+public class Person {
+    private String name;
+    private int age;
+    private boolean married;
+
+    public Person(String name, int age, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.married = married;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+
+}
